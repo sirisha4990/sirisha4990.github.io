5846075b13017cab1d0f504572c8312ef526bec0
sirisha4990 <ThatGirlSriracha94@gmail.com>
2024-07-11 16:37:47 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,1 +1,12 @@
 console.log("basicMap.js");
+
+//CPCC Coordinates: 35.2176665 N, 80.831473 W ->35.2176665, -80.831473
+
+let map = l.map("map",{
+  center:[35.2176665, -80.831473],
+  zoom: 16
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
