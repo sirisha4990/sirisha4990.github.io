aca5208186541b119f5902bc99cb447e399a0910
sirisha4990 <ThatGirlSriracha94@gmail.com>
2024-07-11 16:43:57 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -8,5 +8,6 @@ let map = l.map("map",{
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
