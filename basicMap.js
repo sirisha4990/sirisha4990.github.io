2375642139f50f686cd4754a3d2628af7b0f80ca
sirisha4990 <ThatGirlSriracha94@gmail.com>
2024-07-16 16:31:31 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -14,5 +14,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 L.marker([35.2176665, -80.831473], {
   title:"What's up from CPCC!"
-}).bindPopup("Hello from CPCC!")
+}).bindPopup(`
+<center>
+<h2>Hello from CPCC!</h2>
+<hr>
+<a href="https://www.cpcc.edu"><Click Here to Visit CPCC's Website</a><br>
+<iframe width="280" height="160" src="https://www.youtube.com/embed/nKrZk6cOcEU?si=LChpk8VL5ZQkoD4-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</center>
+`)
 	.addTo(map);
