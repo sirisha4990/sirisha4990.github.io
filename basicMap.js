79fba0f804ff6c723f2a632e96797ccdb17dca2b
sirisha4990 <ThatGirlSriracha94@gmail.com>
2024-07-16 16:43:48 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -20,6 +20,8 @@ L.marker([35.2176665, -80.831473], {
 <hr>
 <a href="https://www.cpcc.edu"><Click Here to Visit CPCC's Website</a><br>
 <iframe width="280" height="160" src="https://www.youtube.com/embed/nKrZk6cOcEU?si=LChpk8VL5ZQkoD4-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<hr>
+<p>[https://youtu.be/nKrZk6cOcEU?si=iIK_TLr9Enyh7RNw]</p>
 </center>
 `)
 	.addTo(map);
