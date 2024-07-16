ea1fc339dc4d93756a91bb17df7c6ee0c00c2faf
sirisha4990 <ThatGirlSriracha94@gmail.com>
2024-07-16 15:48:20 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -8,8 +8,8 @@ let map = L.map("map",{
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker(35.2176665, -80.831473), {
